e1c18e1af45ae8439ff9db7e26663b5afff7b253
felis <foreverddong@outlook.com>
2025-11-30 15:11:31 -0800

    add hello controller

---
+++ MathAPI/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet("hello")]
+    public string Hello()
+    {
+        return "Hello, maki!";
+    }
+
+    [HttpGet("echo")]
+    public string Echo([FromQuery] string str)
+    {
+        return $"You've entered: {str} in the query"; 
+    }
+}
